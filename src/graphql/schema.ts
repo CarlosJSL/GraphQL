0fa2db02adf7ecb2781690e6ae60ee96316f974b
Carlos josé <carlosjsl95@gmail.com>
2017-11-27 17:03:20 -0300

    Implemmeting first mutation

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -19,14 +19,24 @@ const typeDefs = `
         name: String!
         email: String!   
     }
-    type Query{
+    type Query {
         allUsers: [User!]!   
+    }
+    type Mutation {
+        createUser(name: String!, email: String!): User
     }
 `;
 
 const resolvers = {
     Query: {
         allUsers:() => users
+    },
+    Mutation:{
+        createUser:(parent,args) => {
+            const newUser  = Object.assign({id:users.length+1},args)
+            users.push(newUser);
+            return newUser;
+        }
     }
 }
 
