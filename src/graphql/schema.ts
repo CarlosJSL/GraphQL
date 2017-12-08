1e5378362ca312af3d00a64943ad032a1fecda4d
Carlos josé <carlosjsl95@gmail.com>
2017-12-07 22:04:49 -0300

    Add connect to the database

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,43 +1,10 @@
 import {makeExecutableSchema} from 'graphql-tools'
 
-const users: any[] = [
-    {
-        id: 1,
-        name: "Carlos",
-        email:"carlosjsl95@gmail.com"
-    },
-    {
-        id: 2,
-        name: "Joao",
-        email:"joao@gmail.com"
+const SchemaDefinition = `
+    typeSchema{
+            query: Query
+            mutation: Mutation
     }
-];
-
-const typeDefs = `
-    type User {
-        id: ID!
-        name: String!
-        email: String!   
-    }
-    type Query {
-        allUsers: [User!]!   
-    }
-    type Mutation {
-        createUser(name: String!, email: String!): User
-    }
-`;
-
-const resolvers = {
-    Query: {
-        allUsers:() => users
-    },
-    Mutation:{
-        createUser:(parent,args) => {
-            const newUser  = Object.assign({id:users.length+1},args)
-            users.push(newUser);
-            return newUser;
-        }
-    }
-}
+`
 
 export default makeExecutableSchema({typeDefs,resolvers})
